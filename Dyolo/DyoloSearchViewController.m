4d654e5d5f436f9a223cecdf2d70805502f829f8
Joyann <joyann.init@gmail.com>
2014-11-30 15:47:11 +0800

    增加SearchBar和TableView.设置ContentInset. 增加Tap手势和滑动ScrollView来隐藏键盘.

--- Dyolo/DyoloSearchViewController.m
+++ Dyolo/DyoloSearchViewController.m
@@ -8,30 +8,100 @@
 
 #import "DyoloSearchViewController.h"
 
-@interface DyoloSearchViewController ()
+@interface DyoloSearchViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation DyoloSearchViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
+    
+    // Add tap gesture to hide keyboard.
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard:)];
+    tapGestureRecognizer.cancelsTouchesInView = NO;
+    [self.tableView addGestureRecognizer:tapGestureRecognizer];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - HideKeyboard
+
+- (void)hideKeyboard:(UIGestureRecognizer *)gestureRecognizer
+{
+    [self dismissKeyboard];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)dismissKeyboard
+{
+    [self.searchBar resignFirstResponder];
 }
-*/
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self dismissKeyboard];
+}
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
